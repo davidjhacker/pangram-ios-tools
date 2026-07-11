a1c5181bbe9babd421b4f3b70b00ddc7c1725330
davidjhacker <dhacker2019@gmail.com>
2026-07-10 21:24:47 -0400

    Initial Commit

---
+++ Pangram iOS Tools (Unofficial)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pangram iOS Tools (Unofficial)
+//
+//  Created by David Hacker on 7/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pangram iOS Tools (Unofficial)/Pangram_iOS_Tools__Unofficial_App.swift
@@ -0,0 +1,17 @@
+//
+//  Pangram_iOS_Tools__Unofficial_App.swift
+//  Pangram iOS Tools (Unofficial)
+//
+//  Created by David Hacker on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Pangram_iOS_Tools__Unofficial_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
